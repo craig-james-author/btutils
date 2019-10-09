39d701f7530270fd479367b19cf3c09cf54ce7c3
Craig James <cjames@craigs-mbp.home>
2019-10-09 09:52:22 -0700

    All examples: Modified setup() function to have all relevant functions you might want to use along with comments explaining each.
    Craig Examples/ProximityVolumeResume/ProximityVolumeResume.ino
      Renamed.
    
    Craig Examples/jail/ProximityVolume/ProximityVolume.ino
      Moved to "jail". Not a good example.

--- Craig Examples/TouchFadeInReleaseFadeOut/TouchFadeInReleaseFadeOut.ino
+++ Craig Examples/TouchFadeInReleaseFadeOut/TouchFadeInReleaseFadeOut.ino
@@ -14,14 +14,39 @@ BtUtils *bt;
 void setup() {
   bt = BtUtils::setup(&sd, &MP3player);
 
-  // Set the fade-in time in milliseconds (e.g. 1000 is 1 second). When a track is started,
-  // the volume is initially zero, and it takes this much time to reach full volume.
+  // This sets an "idle timeout" -- if nothing hapens for this length of time,
+  // it clears out the resume feature so that the next time you call
+  // bt->resume(), it will start the track over instead.  The timeout is in
+  // seconds.
 
-  bt->setFadeInTime(2000);    // 2 second fade-in
+  bt->startOverAfterNoTouchTime(30);	// 30 seconds 
 
-  // Same for the fade-out time.
 
+  // Set the output volume. This ranges from zero (silent) to 100 (full
+  // volume).  The default is 100 (i.e. if you don't call this function at
+  // all, the volume will be 100%).
+
+  bt->setVolume(100);
+
+
+  // Set the fade-in and fade-out times in milliseconds (e.g. 1000 is 1
+  // second). When a track is started, the volume is initially zero, and it
+  // takes this much time to reach the volume. Note that whatever you set
+  // the volume to (see above) is the target that the fade-in is heading
+  // for in the specified time.
+
+  bt->setFadeInTime(1000);    // 1 second fade-in
   bt->setFadeOutTime(2000);   // 2 second fade-out
+
+
+  // Set the touch sensitivity. Low values make it very sensitive (i.e. it
+  // will trigger a touch even when your hand is nearby), and high valuse
+  // make it less sensitive (i.e. you have to actually touch the contact).
+  // The first number is touch, the second number is release. Touch <i>must</i>
+  // be greater than release.
+
+  bt->setTouchReleaseThreshold(20, 10);
+
 }
 
 
